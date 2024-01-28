9aba6e8f02e1743a48c1fbc55355f4912f2b5ee5
Buspe <93484404+AM-Dominik@users.noreply.github.com>
2024-01-28 11:07:25 +0100

    28.01.2024 Aktualizacja pliku facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -1,21 +1,17 @@
 from deepface import DeepFace
 import pandas as pd
 import os
+from pathlib import Path
 
 file_path = os.getcwd().replace("\\", "/")
 
-# wskazanie pojedynczego zdjęcia do porównania
-img_path = file_path + "/img.jpg"
-
-# wskazanie miejsca bazy zdjęć (w postaci folderu), w którym znajdują sie zdjęcia do porównania
-db_path = file_path + "/face"
-
-def compareFaces():
+def compareFaces(model_name, img_path, db_path): #"VGG-Face", "OpenFace"
 
     # porównanie zdjęcia wejciowego ze zbiorem zdjęć znajdującymi się w podanym folderze
     # zwraca wszystkie podobne zdjęcia do podanego
     try:
-        result = DeepFace.find(img_path, db_path)
+        result = DeepFace.find(img_path, db_path,
+        model_name = model_name)
     except ValueError:
         # w przypadku nierozpoznania twarzy
         return ""
@@ -35,6 +31,6 @@ def compareFaces():
     # wypisanie imienia na podstawie nazwy zdjęcia
     return name
 
-def createFaceLabelFile():
+def createFaceLabelFile(model_name, img_path, db_path):
     with open('name.txt', 'w') as text_file:
-        text_file.write(compareFaces())
+        text_file.write(compareFaces(model_name, img_path, db_path))
